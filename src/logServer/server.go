2aef404fb40c926ba77153045cd7c4b673b747e3
aoesg <2447638380@qq.com>
2024-01-05 05:15:43 -0800

    wzlog

--- src/logServer/server.go
+++ src/logServer/server.go
@@ -3,6 +3,7 @@ package logServer
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/gin-gonic/gin"
@@ -10,6 +11,8 @@ import (
 
 	"cs255/logLayer/accessRedis"
 )
+
+var ssf2clear []string
 
 var logNode_id int
 var replica_id_slice []int
@@ -43,6 +46,7 @@ func HttpServer(ip string, port string) {
 
 	router.GET("/write", set_handler)
 	router.GET("/read", get_handler)
+	router.GET("/clear", clear_handler)
 
 	// 默认在 8080 端口启动服务，除非定义了一个 PORT 的环境变量。
 	router.Run(ip + ":" + port)
@@ -65,9 +69,18 @@ func set_handler(c *gin.Context) {
 	ssf_id := c.Param("ssf_id")
 	step_id := c.Param("step_id")
 
-	fmt.Printf("ssf_id=%s write %s:%s in %s at step=%s\n", ssf_id, key, value, db_address, step_id)
+	if db_address == "" ||
+		key == "" ||
+		value == "" ||
+		ssf_id == "" ||
+		step_id == "" {
+		resp_wzLog(c, "", -1, "set fail, param invalid")
+		return
+	}
 
-	// Start wzLog
+	fmt.Printf("ssf_id=%s set %s:%s in %s at step=%s\n", ssf_id, key, value, db_address, step_id)
+
+	// Start set wzLog
 
 	// get本地(db_address:key:ssf_id:step_id)
 	log_key := fmt.Sprintf("%s:%s:%s:%s", db_address, key, ssf_id, step_id)
@@ -82,45 +95,33 @@ func set_handler(c *gin.Context) {
 		if value != "" {
 			// if value 存在
 			resp_wzLog(c, "", 1, "set success")
+			return
 		} else {
 			// if 只有 version
 			db_key := fmt.Sprintf("%s:%s", key, version)
-			err = accessRedis.Set_v1(db_address, db_key, value) // set 远程db (key:version)
+			_ = accessRedis.Set_v1(db_address, db_key, value) // set 远程db (key:version)
 			log_record := fmt.Sprintf("%s:%s", value, version)
-			err = accessRedis.Set_v1(local_redis_address, log_key, log_record) // set 本地 "value:version"
+			_ = accessRedis.Set_v1(local_redis_address, log_key, log_record) // set 本地 "value:version"
 
 			resp_wzLog(c, "", 1, "set success")
+			return
 		}
 	} else {
 		// if value&version 都不存在，本地无记录
 		db_version_key := key
 		version, _ := accessRedis.Incr_v1(db_address, db_version_key) // incr 远程db (key)
 		log_record_onlyVersion := fmt.Sprintf(":%s", version)
-		err = accessRedis.Set_v1(local_redis_address, log_key, log_record_onlyVersion) // set 本地 ":version"
+		_ = accessRedis.Set_v1(local_redis_address, log_key, log_record_onlyVersion) // set 本地 ":version"
 
 		db_key := fmt.Sprintf("%s:%s", key, version)
-		err = accessRedis.Set_v1(db_address, db_key, value) // set 远程db (key:version)
+		_ = accessRedis.Set_v1(db_address, db_key, value) // set 远程db (key:version)
 		log_record := fmt.Sprintf("%s:%s", value, version)
-		err = accessRedis.Set_v1(local_redis_address, log_key, log_record) // set 本地 "value:version"
+		_ = accessRedis.Set_v1(local_redis_address, log_key, log_record) // set 本地 "value:version"
 
 		resp_wzLog(c, "", 1, "set success")
-
+		return
 	}
-
-	version, _ := accessRedis.Incr_v1(db_address, key)
-
-	if value != "" {
-		c.JSON(http.StatusServiceUnavailable, gin.H{
-			"value":  value,
-			"status": "success",
-		})
-	}
-
-	db_key := fmt.Sprintf("%s:%s", db_address, key, ssf_id, step_id)
-
-	err := accessRedis.Set_v1(db_address, key, value)
-
-	// Start wzLog
+	// End set wzLog
 }
 
 func get_handler(c *gin.Context) {
@@ -130,44 +131,71 @@ func get_handler(c *gin.Context) {
 	ssf_id := c.Param("ssf_id")
 	step_id := c.Param("step_id")
 
-	// Start wzLog
-
-	fmt.Printf("ssf_id=%s read %s at step=%s\n", ssf_id, key, step_id)
-
-	version, err := accessRedis.Get_v1(local_redis_address, key) //从本地获取version
-	// if version == ""{
-	// 	version
-	// }
-
-	log_key := fmt.Sprintf("%s:%s:%s:%s", key, version, ssf_id, step_id)
-	db_key := fmt.Sprintf("%s:%s", key, version)
-
-	value, err := accessRedis.Get_v1(local_redis_address, log_key)
-
-	if value == "" {
-		value, err = accessRedis.Get_v1(db_address, db_key) // 远程数据库
+	if db_address == "" ||
+		key == "" ||
+		// value == "" ||
+		ssf_id == "" ||
+		step_id == "" {
+		resp_wzLog(c, "", -1, "set fail, param invalid")
+		return
 	}
 
-	var status string
-	if err == redis.Nil {
-		status = "notExist"
+	fmt.Printf("ssf_id=%s get %s in %s at step=%s\n", ssf_id, key, db_address, step_id)
+
+	// Start get wzLog
+
+	// get本地(db_address:key:ssf_id:step_id)
+	log_key := fmt.Sprintf("%s:%s:%s:%s", db_address, key, ssf_id, step_id)
+	value_version_string, err := accessRedis.Get_v1(local_redis_address, log_key) // get 本地
+	value_version_arr := strings.Split(value_version_string, ":")
+
+	// if 本地(db_address:key:ssf_id:step_id) 存在
+	if err != redis.Nil {
+		value := value_version_arr[0]
+
+		resp_wzLog(c, value, 1, "set success")
 	} else {
-		status = "success"
-		err = accessRedis.Set_v1(local_redis_address, log_key, value)
+		// if 本地(db_address:key:ssf_id:step_id) 为空
+		db_version_key := key
+		version, err := accessRedis.Get_v1(db_address, db_version_key) // get 远程db (key), 获取某一个version
+
+		if err == redis.Nil {
+			// if 远程db 没有key的version
+			resp_wzLog(c, "", 1, "get success, but notExisted")
+			return
+		}
+
+		version_int, _ := strconv.Atoi(version)
+
+		// if 远程db 存在key的version
+		for {
+			db_key := fmt.Sprintf("%s:%d", key, version_int)
+			value, err := accessRedis.Get_v1(db_address, db_key) // get 远程db (key:version)
+
+			if err != redis.Nil {
+				// if 远程db 存在对应 (key:version) 的 value
+				log_record := fmt.Sprintf("%s:%d", value, version_int)
+				_ = accessRedis.Set_v1(local_redis_address, log_key, log_record) // set 本地 "value:version"
+				resp_wzLog(c, value, 1, "get success")
+				return
+			}
+
+			version_int = version_int - 1
+
+			if version_int == 0 {
+				// if 远程db 没有存在的version（减到0都不存在）
+				resp_wzLog(c, "", 1, "get success, but notExisted")
+				return
+			}
+		}
 	}
+	// End get wzLog
+}
 
-	// End wzLog
+func clear_handler(c *gin.Context) {
+	ssf_id := c.Param("ssf_id")
 
-	if err != nil {
-		c.JSON(http.StatusServiceUnavailable, gin.H{
-			"value":   value,
-			"version": version,
-			"status":  status,
-		})
-	}
+	ssf2clear = append(ssf2clear, ssf_id)
 
-	c.JSON(http.StatusOK, gin.H{
-		"value":  value,
-		"status": status,
-	})
+	resp_wzLog(c, "", 1, "clear success")
 }
